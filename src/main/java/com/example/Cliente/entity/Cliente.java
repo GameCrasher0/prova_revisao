282892310bf0f80c79d6acddb7ae53369aa5ea8d
GameCrasher <andrelwolski@hotmail.com>
2022-11-18 19:26:27 -0300

    commit arrobas

--- src/main/java/com/example/Cliente/entity/Cliente.java
+++ src/main/java/com/example/Cliente/entity/Cliente.java
@@ -4,8 +4,47 @@ import java.util.List;
 
 public class Cliente {
 
+    private Long id;
     private String nomeCompleto;
     private String profissao;
     private Double receita;
     private List<Endereco> enderecos;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public Double getReceita() {
+        return receita;
+    }
+
+    public void setReceita(Double receita) {
+        this.receita = receita;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
 }
+
