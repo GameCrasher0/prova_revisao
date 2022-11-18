d22d8a020fa7da72f920326a6ae0127da424324c
GameCrasher <andrelwolski@hotmail.com>
2022-11-18 20:42:43 -0300

    utils

--- src/main/java/com/example/Cliente/controller/ClienteController.java
+++ src/main/java/com/example/Cliente/controller/ClienteController.java
@@ -1,4 +1,22 @@
 package com.example.Cliente.controller;
 
+import com.example.Cliente.entity.Cliente;
+import com.example.Cliente.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping
+    @ResponseBody
+    public Cliente create(@RequestBody Cliente cliente){
+        Cliente clienteSaved = clienteRepository.save(cliente);
+        return clienteSaved;
+    }
+
 }
